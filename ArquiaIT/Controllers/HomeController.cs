7fcf9394331b4371f9b9785ae9c4fa00ef491f51
Sebastian <Sebastiannuss@gmail.com>
2019-05-15 22:19:58 -0300

    Change Index Page to startup

--- ArquiaIT/Controllers/HomeController.cs
+++ ArquiaIT/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace ArquiaIT.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
